30cbde2d502f165e8f9d8602fbafbe67dcf228e9
John Flores <johnflores1991@hotmail.com>
2023-09-28 14:33:04 -0500

    completed steps up to 5

--- app.py
+++ app.py
@@ -26,4 +26,21 @@ def begin():
 @app.get("/questions/<int:id>")
 def questions(id):
     question = survey.questions[id]
-    return render_template("question.html", question=question)
+    return render_template("question.html", question=question, id=id)
+
+@app.post('/answer/<int:id>')
+def answers(id):
+    answer = request.form.get('answer')
+    responses.append(answer)
+    id+=1
+    if id == len(survey.questions):
+        #TODO: Step 5: display Q/A in a <ul>
+        for question in survey.questions:
+            flash(f'{question}')
+        return redirect('/thankyou')
+
+    return redirect(f'/questions/{id}')
+
+@app.get('/thankyou')
+def completion():
+    return render_template('completion.html', questions=questions, answers=answers)
